8c2a1563d3d5e497e5d0b5cd2211c4e92d54950c
Jithin Manilal <jithinmanilal@gmail.com>
2023-04-30 13:20:23 +0530

    updated allowed hosts

--- cakesmiths/settings.py
+++ cakesmiths/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-buxwebms.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
